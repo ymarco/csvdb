4a544c49490bea59ef52df6495cf8689418fe73f
yoav marco <yoavm448@gmail.com>
2019-03-30 14:39:17 +0300

    forgot to commit DBVar on last commit

---
+++ schema/DBVar.java
@@ -0,0 +1,13 @@
+package schema;
+
+public class DBVar {
+	public long i;
+	public String s;
+	public double f;
+	public long ts; // ts is treated as an UNSIGNED long
+
+	public static final long   NULL_INT = Long.MIN_VALUE;
+	public static final String NULL_STRING = "";
+	public static final double NULL_FLOAT = Double.NEGATIVE_INFINITY;
+	public static final long   NULL_TS = 0;
+}
